3da56ca7553f84cdd2e3e20e8587cde6a743c45e
HYH! <huangyinhaow@outlook.com>
2023-04-10 18:38:34 +0800

    lint

--- src/elements/quote.js
+++ src/elements/quote.js
@@ -8,7 +8,7 @@ export function Quote() {
       <div className="colums">
         <div className={`box column is-10 is-offset-1 has-background-black-bis has-text-white ${fontMS.className}`}>
           <div className="is-size-3 ml-5 mt-2">Quote</div>
-          <div className="is-size-1 has-text-centered">"Don't be evil"</div>
+          <div className="is-size-1 has-text-centered">Don&lsquot be evil</div>
           <div className="is-size-3 has-text-right mr-5 mb-2">- Google</div>
         </div>
       </div>
